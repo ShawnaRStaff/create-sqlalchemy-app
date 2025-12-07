2ccf36433718e4fa94b3a7dd8b90d9002de53d40
Shawna <shawnastaff@gmail.com>
2025-12-06 17:56:13 -0800

    Fix Jinja2 template syntax and remove old files
    - Use {% raw %} blocks in script templates to avoid curly brace conflicts
    - Remove old monolithic setup.py (replaced by package)
    - Remove definition_scripts/ (templates now in src/)
    - Add .gitignore for Python/IDE files

--- definition_scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-from .data_import_definition_script import DATA_IMPORT_DEFINITION_SCRIPT
-from .model_definitions_script import MODEL_DEFINITIONS_SCRIPT
-
-
-__all__ = [
-    "DATA_IMPORT_DEFINITION_SCRIPT",
-    "MODEL_DEFINITIONS_SCRIPT"
-]
